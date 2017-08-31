68fc04ae1573cb33c262b356ae3c457fbff4b2b5
pushpendraraj <rajput.pushpendra61@gmail.com>
2017-08-31 22:12:44 +0530

    updated controller webservies

--- app/Controller/WebserviceController.php
+++ app/Controller/WebserviceController.php
@@ -6,6 +6,7 @@ class WebserviceController extends AppController {
 
     function beforeFilter(){
         parent::beforeFilter();
+		//Configure::write('debug', 2);
         $this->Auth->allow(array('get_countries','get_categories','getPageInfo','getip','sendApplyInfo','get_languages','get_slides','get_sub_categories','get_products','get_modifiers','get_options','get_suboptions','getImagePath','get_all_categories_data','getItemData','placeOrder','getStoreList','getStoresFromPostalCode', 'getStoresFromLatLong','getStoreDetails','login','getTwitterFeeds','getInstagramPost','getCountryStores','saveFavItem','getCitiesSuggestion','getFBFeed','getIGFeed','getPrefrences','signUp', 'getFav', 'getFavItemData','applyCoupon','getFavOrderData','getProfile','sendCateringInfo','sendContactInfo','sendCareerInfo','getOrderHistory','updateProfile','getProductNameByPlu','getModifierName','updatePrefrence','addAddress','deleteAddress','editAddress','setAsDefault','getUserPrefreces','getAreaSuggestion','testUrl', 'getStoreDetailsByStoreId','forgot_password','reset_password','getReOrderData'));
     }
 
